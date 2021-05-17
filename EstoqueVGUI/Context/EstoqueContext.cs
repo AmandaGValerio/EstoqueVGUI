6aa6b30163915a9e47048fff8260739cf486b915
AmandaGValerio <valeriogamanda@gmail.com>
2021-05-17 16:41:00 -0300

    Configuracao de dependencias
    configuração das dependencias para funcionamento do MySQL

--- EstoqueVGUI/Context/EstoqueContext.cs
+++ EstoqueVGUI/Context/EstoqueContext.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MySql.Data;
-//using Pomelo.EntityFrameworkCore;
+
 
 namespace Estoque
 {
@@ -10,8 +10,8 @@ namespace Estoque
     {
         private const string connectionString = "Server=localhost;Database=estoque;User=root;";
  
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseMySql("server=localhost;port=3306;database=estoque;uid=root;persistsecurityinfo=True;", new MySqlServerVersion(new Version(8, 0, 22)));
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    => optionsBuilder.UseMySQL();
 
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Peca> Pecas { get; set; }
